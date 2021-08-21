b66e9d9ef27409a1801d52cb4aa54e0e010fd8b1
HebaAhmad1 <heba.tanani1@gmail.com>
2021-08-21 22:22:26 +0300

    update js gor welcome page relates #5

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ button.addEventListener("click", logIn);
 function logIn() {
   window.location.href = "./home/home.html";
 }
+
+let instructBtn = document.querySelector("#instructions");
+
+instructBtn.addEventListener("click", (e) => {
+	alert("Welcome to Rock-Paper-Scissors! Have a good time :) ");
+	alert("Please press the Rock, Paper, or Scissors button to begin.");
+});
+function play() {
+  var audio = new Audio('./assets/rise-and-shine.mp3');
+  audio.play();
+}
